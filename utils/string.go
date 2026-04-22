82d40cfd1233e331b69397996e300f3de17f6c6f
zero <zhangji2@foxmail.com>
2026-04-22 16:47:35 +0800

    init

---
+++ utils/string.go
@@ -0,0 +1,27 @@
+package utils
+
+import "strings"
+
+// HasPrefix 判断字符串 s 是否以前缀 prefix 开头。
+func HasPrefix(s string, prefix string) bool {
+	if s == "" || prefix == "" {
+		return false
+	}
+	return strings.HasPrefix(s, prefix)
+}
+
+// HasSuffix 判断字符串 s 是否以后缀 suffix 结尾。
+func HasSuffix(s string, suffix string) bool {
+	if s == "" || suffix == "" {
+		return false
+	}
+	return strings.HasSuffix(s, suffix)
+}
+
+// InString 判断字符串 s 是否包含子字符串 substr。
+func InString(s string, substr string) bool {
+	if s == "" || substr == "" {
+		return false
+	}
+	return strings.Contains(s, substr)
+}
